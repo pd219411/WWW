2759e5b857b60825a378db257f9c882e00c876b5
Piotr Daszkiewicz <pd219411@students.mimuw.edu.pl>
2015-04-16 21:54:16 +0200

    fun with urls and views

--- daszek_project/daszek_project/urls.py
+++ daszek_project/daszek_project/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'daszek_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
