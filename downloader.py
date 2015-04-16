3ce5b6803160ad92acf4b755bd3982eafd880877
Piotr Daszkiewicz <pd219411@students.mimuw.edu.pl>
2015-04-16 16:27:44 +0200

    fixed case for leaves on different depth

--- downloader.py
+++ downloader.py
@@ -5,12 +5,10 @@ from bs4 import BeautifulSoup
 
 #params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
 directory_url = "http://prezydent2010.pkw.gov.pl/PZT/PL/WYN/W/"
-#file_url = "index.htm"
-file_url = "020100.htm"
 
-class DataExtractor:
-	def extract_data(self):
-		pass
+file_url = "index.htm"
+#file_url = "020100.htm"
+#file_url = "http://prezydent2010.pkw.gov.pl/PZT/PL/WYN/P/020000/020702_1.html"
 
 def soup_html_from_url(url):
 	stream = urllib.urlopen(url)
@@ -23,13 +21,14 @@ def extract_link_from_row(soup_row):
 
 def extract_info_from_row(soup_row):
 	cells = soup_row.findAll("td")
-	print cells[1].string
-	print cells[2].string
-	return "EMPTY DATA"
+	name = cells[1].string
+	votes = cells[2].string
+	return (name, votes)
 	
-
 def extract_data_from_html(soup_html):
 	table = soup_html.find("table", {"id" : "s0"})
+	if table is None:
+		return None
 
 	body = table.find_all("tbody")[0]
 	rows = body.find_all("tr", {})
@@ -37,25 +36,24 @@ def extract_data_from_html(soup_html):
 	data_list = []
 
 	for row in rows:
-		print "=================================== ROW >>"
-		print extract_info_from_row(row)
-		print extract_link_from_row(row)
 		data_list.append((extract_link_from_row(row), extract_info_from_row(row)))
-		print "===================================ZZZZZZZZ"
 
 	return data_list
 
 def data_from_url(url):
 	return extract_data_from_html(soup_html_from_url(url))
 
-def crawl_page(base_url, file_url, level):
+def crawl_page(base_url, file_url, level, location):
 	page_url = urlparse.urljoin(directory_url, file_url)
-	print(page_url)
-	
-	data = data_from_url(page_url)
-	print data
-	if level > 0:
-		for (link, info) in data:
-			crawl_page(base_url, link, level - 1)
 
-crawl_page(directory_url, file_url, 1)
+	data = data_from_url(page_url)
+	if data is None:
+		print location
+	else:
+		for (link, info) in data:
+			(name, votes) = info
+			#location_subzone = location + ", " + name + ", " + link
+			location_subzone = location + ", " + name
+			crawl_page(base_url, link, level + 1, location_subzone)
+
+crawl_page(directory_url, file_url, 0, "Polska")
