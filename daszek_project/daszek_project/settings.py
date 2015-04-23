c8a76cd4eb7e7e2f842e07dc72738dfcf7ae84e6
Piotr Daszkiewicz <pd219411@students.mimuw.edu.pl>
2015-04-23 10:59:16 +0200

    wybory model

--- daszek_project/daszek_project/settings.py
+++ daszek_project/daszek_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'polls',
+    'wybory',
 )
 
 MIDDLEWARE_CLASSES = (
--- daszek_project/wybory/models.py
+++ daszek_project/wybory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Gmina(models.Model):
+	nazwa = models.TextField()
+	def __unicode__(self):
+		return self.nazwa
+
+class Obwod(models.Model):
+	gmina = models.ForeignKey(Gmina)
+	nazwa = models.TextField()
+	data_modyfikacji = models.DateTimeField(auto_now = True)
+	kart_do_glosowania = models.IntegerField(default=0)
+	wyborcow = models.IntegerField(default = 0)
+	def __unicode__(self):
+		return self.nazwa
